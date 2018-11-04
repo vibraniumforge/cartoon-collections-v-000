7c337cdb542328905739d3ae522858ed87fc095b
Greg Morgan <gregm9999@gmail.com>
2018-11-04 02:48:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |call| call.capitalize + '!' }
+  planeteer_calls.map {|call| call.capitalize + '!' }
 end
 
 def long_planeteer_calls(calls_long)
